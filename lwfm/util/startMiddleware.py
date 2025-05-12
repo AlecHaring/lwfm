c1792419a63ecf90b55d5f19ab0bac712be3cba9
Alec Haring <alecsharing@gmail.com>
2025-05-12 15:14:40 -0400

    Added `startMiddleware`

---
+++ lwfm/util/startMiddleware.py
@@ -0,0 +1,17 @@
+"""
+start the lwfm middleware
+"""
+
+#pylint: disable = invalid-name
+
+from lwfm.midware.impl.LwfmEventSvc import app
+
+def startMidware():
+    """
+    Run the Flask app for lwfm middleware.
+    """
+    app.run(host="0.0.0.0", port=3000)
+
+
+if __name__ == "__main__":
+    startMidware()
